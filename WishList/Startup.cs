2d88648ac26b6858d353be6a53524d69166e72b8
Arun P Nair <arunpnair@hotmail.com>
2018-06-10 18:47:12 +0530

    Updated the response text
    Updated the response text

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -23,7 +23,7 @@ namespace WishList
         {
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Sun God!");
             });
         }
     }
